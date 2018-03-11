7281b491fc8de5a13ce833ecd491d2584a663abb
Caleb Bassi <calebjbassi@gmail.com>
2018-03-11 11:58:57 -0700

    Refactor

--- gmusicchecker.py
+++ gmusicchecker.py
@@ -1,80 +1,85 @@
-from gmusicapi import Mobileclient
-import argparse
 import os
+import argparse
+from gmusicapi import Mobileclient
 
-# Adds arguments to command line
 
-parser = argparse.ArgumentParser()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username', action='store',
+                        help='Stores username')
+    parser.add_argument('password', action='store',
+                        help='Stores password')
+    return parser.parse_args()
 
-parser.add_argument('username', action='store',
-                    help='Stores username')
-parser.add_argument('password', action='store',
-                    help='Stores password')
 
-authentication = parser.parse_args()
+def authenticate(username, password):
+    api = Mobileclient()
+    api.login(username, password, Mobileclient.FROM_MAC_ADDRESS)
+    return api
 
-# Logs in and retrieves library
 
-api = Mobileclient()
-logged_in = api.login(authentication.username, authentication.password, Mobileclient.FROM_MAC_ADDRESS)
+def touch_file(name):
+    """Creates a file if it doesn't exist"""
+    if not os.path.exists(name):
+        print(f'File \'{name}\' not found. Creating a new one.')
+        with open(name, 'w'):
+            pass
 
-library = api.get_all_songs()
 
-# Creates the library.txt if not exist
+def get_old_songs():
+    with open('library.txt', encoding='utf-8') as file:
+        return [line.strip() for line in file]
 
-if not os.path.exists('library.txt'):
-    f = open('library.txt', 'w')
-    f.close()
-    print('No library.txt found. Creating a new one.')
 
-# Old library
+def get_new_songs(library):
+    new_songs = []
+    for song in library:
+        title, artist, album, id = song['title'], song['artist'], song['album'], song['id']
+        new_songs.append(f'{title} artist: {artist} album: {album} id: {id}')
+    return new_songs
 
-oldsongs = []
-with open("library.txt", encoding='utf-8') as file:
-    for line in file:
-        line = line.strip()
-        oldsongs.append(line)
 
-# New library
+def compare(old, new):
+    """Compares old library with new library and prints the differences"""
+    print('Checking missing songs:')
+    for song in old:
+        if song not in new:
+            print('missing song: ' + song)
+    print('-' * 10 + '\nChecking newly added songs:')
+    for song in new:
+        if song not in old:
+            print('new song added: ' + song)
 
-newsongs = []
-for i in library:
-    newsongs.append("{} artist: {} album: {} id: {}".format(str(i['title']), str(i['artist']), str(i['album']), str(i['id'])))
 
-def comp(old, new):
+def update(new_songs):
+    """Overwrites 'library.txt' with new library"""
+    with open('library.txt', 'w', encoding='utf-8') as file:
+        for song in new_songs:
+            file.write(song + '\n')
 
-    # Compares old library with new library
-
-    print('Checking songs missing:')
-    for val in old:
-        if val not in new:
-            print('missing song: ' + val)
-    print('-' * 10 + '\nChecking new songs added:')
-    for val in new:
-        if val not in old:
-            print('new song added: ' + val)
-
-def update():
-
-    # Overwrites existing library with new library
-
-    f = open('library.txt', 'w', encoding='utf-8')
-
-    for i in library:
-        f.write("{} artist: {} album: {} id: {}\n".format(str(i['title']), str(i['artist']), str(i['album']), str(i['id'])))
-
-    f.close()
 
 def main():
+    args = parse_args()
+    username, password = args.username, args.password
 
-    comp(oldsongs, newsongs)
-    print('-' * 10 + '\nPress 1 for updating library. Otherwise close the window to exit')
-    updatelibrary = input()
-    if updatelibrary == '1':
-        update()
+    api = authenticate(username, password)
+    library = api.get_all_songs()
+
+    touch_file('library.txt')
+
+    old_songs = get_old_songs()
+    new_songs = get_new_songs(library)
+
+    compare(old_songs, new_songs)
+
+    print('-' * 10 + '\nPress 1 to update library. Otherwise close the window to exit')
+    update_library = input()
+    if update_library == '1':
+        update(new_songs)
         print('Finished updating library!')
     else:
         print('Done!')
+
 
 if __name__ == '__main__':
     main()
